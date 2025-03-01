ad4301ddd470f7ce2de26cc03b154f6a3dfd3c74
Raffael Wagner <raffaelwagner@gmail.com>
2025-03-01 14:29:04 -0300

    conteúdo sobre os tipos de dados

--- tipos-variaveis/src/main/java/edu/raffael/javaEstudo/TiposVariaveis.java
+++ tipos-variaveis/src/main/java/edu/raffael/javaEstudo/TiposVariaveis.java
@@ -1,11 +1,18 @@
 package edu.raffael.javaEstudo;
-
 /**
  *
- * @author Pichau
+ * @author Raffael
  */
 public class TiposVariaveis {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+		byte idade = 123; // por convenção, utiliza-se mais o int para idade
+		short ano = 2021;
+		int cep = 21070333; // caso comece com zero, talvez tenha que ser outro tipo
+		long cpf = 98765432109L; // caso comece com zero, talvez tenha que ser outro tipo
+		float pi = 3.14F;
+		double salario = 1275.33;
+        // Variável é uma área de memória, associada a um nome, que pode armazenar valores de um determinado tipo. Um tipo de dado define um conjunto de valores e um conjunto de operações.
+
+        //Constantes são valores armazenados em memória que não podem ser modificados depois de declarados. Esses valores são representados pela palavra reservada final, seguida do tipo.
+	}
 }
